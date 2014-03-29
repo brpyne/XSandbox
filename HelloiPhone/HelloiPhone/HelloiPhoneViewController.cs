d7cb5122bd327ae5ef98a397c16afce34a042cfa
Pyne, Brad <BradPyne@quickenloans.com>
2014-03-28 23:35:57 -0400

    Init Hello World App

---
+++ HelloiPhone/HelloiPhone/HelloiPhoneViewController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.CoreLocation;
+
+namespace HelloiPhone
+{
+	public partial class HelloiPhoneViewController : UIViewController
+	{
+		public HelloiPhoneViewController () : base ("HelloiPhoneViewController", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			alertDelegate = new MyAlertDelegate ();
+			this.locMgr = new CLLocationManager();
+
+			StartLocationUpdates ();
+		}
+
+		protected CLLocationManager locMgr;
+
+		public CLLocationManager LocMgr{
+			get { return this.locMgr; }
+		}
+
+
+		public void StartLocationUpdates()
+		{
+			// we need the user’s permission to use GPS, so we check to make sure they’ve accepted
+			if (CLLocationManager.LocationServicesEnabled) {
+				//set the desired accuracy, in meters
+				LocMgr.DesiredAccuracy = 1;
+				LocMgr.StartUpdatingLocation();
+			} else {
+				Console.WriteLine ("Location services not enabled");
+			}
+		}
+
+
+		public void HandleLocationChanged (object sender, LocationUpdatedEventArgs e)
+		{
+			/*
+			// handle foreground updates
+			CLLocation location = e.Location;
+			IMainScreen ms = mainScreen;
+			ms.LblAltitude.Text = location.Altitude + " meters";
+			ms.LblLongitude.Text = location.Coordinate.Longitude.ToString ();
+			ms.LblLatitude.Text = location.Coordinate.Latitude.ToString ();
+			ms.LblCourse.Text = location.Course.ToString ();
+			ms.LblSpeed.Text = location.Speed.ToString ();
+			Console.WriteLine ("foreground updated");
+			*/
+		}
+
+
+		public class MyAlertDelegate : UIAlertViewDelegate {
+			public override void Clicked (UIAlertView alertView, int buttonIndex)
+			{
+				Console.WriteLine (buttonIndex.ToString () + " clicked");
+			}
+		}
+
+		MyAlertDelegate alertDelegate;
+
+		partial void btnOK(MonoTouch.Foundation.NSObject sender)
+		{
+			UIAlertView alert = new UIAlertView("Hello World! ", "Welcome "+this.txtInput.Text, alertDelegate, "Ok", "Cancel");
+			alert.Show();
+		}
+
+		public override bool ShouldAutorotateToInterfaceOrientation(UIInterfaceOrientation toInterfaceOrientation)
+		{
+			return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+		}
+	}
+}
+
---
+++ HelloiPhone/HelloiPhone/HelloiPhoneViewController.designer.cs
@@ -0,0 +1,29 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace HelloiPhone
+{
+	[Register ("HelloiPhoneViewController")]
+	partial class HelloiPhoneViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtInput { get; set; }
+
+		[Action ("btnOK:")]
+		partial void btnOK (MonoTouch.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (txtInput != null) {
+				txtInput.Dispose ();
+				txtInput = null;
+			}
+		}
+	}
+}
