6a038c74fd76b6bddebcdfe6a6943a843854a18d
Pyne, Brad <BradPyne@quickenloans.com>
2014-03-29 20:41:07 -0400

    Retrieving Closest Placemark and Geocoding to an Address

--- HelloiPhone/HelloiPhone/HelloiPhoneViewController.cs
+++ HelloiPhone/HelloiPhone/HelloiPhoneViewController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Drawing;
+using System.Threading.Tasks;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using MonoTouch.CoreLocation;
-
 
 namespace HelloiPhone
 {
@@ -13,21 +13,15 @@ namespace HelloiPhone
 		{
 		}
 
+		private CLLocation _currentLocation;
+		private CLPlacemark _closestPlacemark;
+
 		public override void DidReceiveMemoryWarning ()
 		{
 			// Releases the view if it doesn't have a superview.
 			base.DidReceiveMemoryWarning ();
 
 			// Release any cached data, images, etc that aren't in use.
-
-		}
-
-		public async void ReverseGeocodeToConsoleAsync (CLLocation location) {
-			var geoCoder = new CLGeocoder();
-			var placemarks = await geoCoder.ReverseGeocodeLocationAsync(location);
-			foreach (var placemark in placemarks) {
-				Console.WriteLine(placemark);
-			}          
 		}
 
 		public override void ViewDidLoad ()
@@ -36,14 +30,10 @@ namespace HelloiPhone
 
 			this.txtOutput.Text = string.Empty;
 
-
-			// screen subscribes to the location changed event
 			UIApplication.Notifications.ObserveDidBecomeActive ((sender, args) => {
 				AppDelegate.Manager.LocationUpdated += HandleLocationChanged;
 			});
 
-			// whenever the app enters the background state, we unsubscribe from the event 
-			// so we no longer perform foreground updates
 			UIApplication.Notifications.ObserveDidEnterBackground ((sender, args) => {
 				AppDelegate.Manager.LocationUpdated -= HandleLocationChanged;
 			});
@@ -51,21 +41,59 @@ namespace HelloiPhone
 
 		public void HandleLocationChanged (object sender, LocationUpdatedEventArgs e)
 		{
-			// handle foreground updates
-			CLLocation location = e.Location;
-			Log(string.Format("{0}, {1}", location.Coordinate.Longitude.ToString (), location.Coordinate.Latitude.ToString ()));
-
-			Console.WriteLine ("foreground updated");
+			CurrentLocation = e.Location;
 		}
 
+		public CLLocation CurrentLocation {
+			get { 
+				return _currentLocation; 
+			}
+			set { 
+				_currentLocation = value;
 
-		private void Log(string message)
+				SetClosestPlacemark (_currentLocation);
+			}
+		}
+
+		public CLPlacemark ClosestPlacemark {
+			get {
+				return _closestPlacemark;
+			}
+			set {
+				_closestPlacemark = value;
+
+				Log (GetCurrentCity (_closestPlacemark));
+			}
+		}
+
+		public async void SetClosestPlacemark (CLLocation location)
+		{
+			var placemarks = await this.ReverseGeocodeAsync (location);
+			ClosestPlacemark = placemarks [0];
+
+		}
+
+		public string GetCurrentCity (CLPlacemark placemark)
+		{
+			return placemark.AddressDictionary.ToString ();
+		}
+
+		public async Task<CLPlacemark[]> ReverseGeocodeAsync (CLLocation location)
+		{
+			var geoCoder = new CLGeocoder ();
+			var placemarks = await geoCoder.ReverseGeocodeLocationAsync (location);
+
+			foreach (var placemark in placemarks) {
+				Console.WriteLine (placemark);
+			}          
+
+			return placemarks;
+		}
+
+		private void Log (string message)
 		{
 			txtOutput.Text += message + Environment.NewLine;
 		}
-
-
 	}
-
 }
 
